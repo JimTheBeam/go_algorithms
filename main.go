5a83af205bce9a59921716d2890eb3d4ddf7ef57
JimTheBeam <24dmitriy@mail.ru>
2021-05-02 01:10:05 +0300

    [v] Selection sort + tests

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	arr := []int{741, 123, 369, 987, 753, 5, 159, 852, 2, 35, 94, 72, 85, 12, 34, 97, 65}
+	arr := []int{-741, 123, 369, -987, 753, -5, 159, -852, -2, 35, -94, 72, -85, 12, 34, -97, 65}
 
 	sorted := sorting.Selection(arr)
 	for _, elem := range sorted {
